3170a9893754d2daf876e65d35087e6042c57dbc
HoJin9622 <kiss0104040@gmail.com>
2021-03-14 14:51:45 +0900

    회원가입

--- src/layouts/App.tsx
+++ src/layouts/App.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
+import loadable from '@loadable/component'
 import { Switch, Route, Redirect } from 'react-router-dom'
-import LogIn from '../pages/Login'
-import SignIn from '../pages/SignUp'
+
+const LogIn = loadable(() => import('../pages/Login'))
+const SignUp = loadable(() => import('../pages/SignUp'))
 
 const App = () => {
   return (
     <Switch>
       <Redirect exact path='/' to='/login' />
       <Route path='/login' component={LogIn} />
-      <Route path='/signup' component={SignIn} />
+      <Route path='/signup' component={SignUp} />
     </Switch>
   )
 }
