d4ece12f8288ea37ade331b0f82cd54241b2ce15
HoJin9622 <kiss0104040@gmail.com>
2021-03-17 19:41:07 +0900

    채팅 리스트 & 채팅 박스

--- src/pages/DirectMessage/index.tsx
+++ src/pages/DirectMessage/index.tsx
@@ -1,7 +1,38 @@
 import React from 'react'
+import gravatar from 'gravatar'
+import { Container, Header } from './styles'
+import useSWR from 'swr'
+import fetcher from '../../utils/fetcher'
+import { useParams } from 'react-router'
+import ChatBox from '../../components/ChatBox'
+import ChatList from '../../components/ChatList'
 
 const DirectMessage = () => {
-  return <div>dm</div>
+  const { workspace, id } = useParams<{ workspace: string; id: string }>()
+
+  const { data: userData } = useSWR(
+    `http://localhost:3095/api/workspaces/${workspace}/users/${id}`,
+    fetcher
+  )
+  const { data: myData } = useSWR(`http://localhost:3095/api/users`, fetcher)
+
+  if (!userData || !myData) {
+    return null
+  }
+
+  return (
+    <Container>
+      <Header>
+        <img
+          src={gravatar.url(userData.email, { s: '24px', d: 'retro' })}
+          alt={userData.nickname}
+        />
+        <span>{userData.nickname}</span>
+      </Header>
+      <ChatList />
+      <ChatBox chat='' />
+    </Container>
+  )
 }
 
 export default DirectMessage
